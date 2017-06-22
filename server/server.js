2002536e92e03a04aa6488d565865008f7e590d3
Nitsan Zohar <nitsan.zohar@startapp.com>
2017-06-22 15:10:39 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+'use strict';
+/*jshint esversion: 6 */
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const movies = require('./movies.json');
+
+
+const app = express();
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/movies', (req, res) => {
+  res.json(movies);
+});
+app.listen(3000, function () {
+  console.log(`app listening on port ${3000}!`);
+});
+
+module.exports = app;
